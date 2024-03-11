b1c4113c9404bad753c4b00534444c9bedcbab66
Eslam Elsayed <111904624+e5lamel5ayed@users.noreply.github.com>
2024-03-11 03:15:19 +0200

    Add files via upload

---
+++ src/data/ProjectData.js
@@ -0,0 +1,146 @@
+// --------------------------------------- Project List
+export const ProjectList = [
+  {
+    img: "/Nasa.jpg",
+    title: "Shop Nasa",
+    description:
+      "Celestial Commerce: Exploring the Universe of NASA-inspired Fashion and Toys.",
+    tech_stack: ["React Js", "Api", "Fontawesome", "BootStrapCss"],
+    github_url: "https://github.com/e5lamel5ayed/Shop_Nasa.git",
+    demo_url: "https://shop-nasa.vercel.app/",
+  },
+  {
+    img: "/Tnw.jpg",
+    title: "Tnw Website",
+    description:
+      "TNW Mobile Showcase: Features Galore, API Amazement",
+    tech_stack: ["React Js", "Api" , "Fontawesome", "BootStrapCss"],
+    github_url: "https://github.com/e5lamel5ayed/TNW.git",
+    demo_url: "https://tnw-six.vercel.app/",
+  },
+  {
+    img: "/Henaa.jpg",
+    title: "Henaa",
+    description:
+      "A Responsive website that shows some pages and sections",
+    tech_stack: ["HTML", "CSS","Js", "BootStrapCss", "Fontawesome"],
+    github_url: "https://github.com/e5lamel5ayed/Henna-Website.git",
+    demo_url: "https://henna-website-xi.vercel.app/",
+  },
+  {
+    img: "/Netflix.jpg",
+    title: "Netflix",
+    description:
+      "A simulation of the Netflix website to display some movies with pictures of them with a smooth design",
+    tech_stack: ["HTML", "CSS","Js" ,"BootStrapCss", "Fontawesome"],
+    github_url: "https://github.com/e5lamel5ayed/Netflix-.git",
+    demo_url: "https://netflix-hij5.vercel.app/",
+  },
+  {
+    img: "/Desny.jpg",
+    title: "Desny",
+    description:
+      "A simulation of the Desny website to display some Cartoon movies with pictures of them with a smooth design",
+    tech_stack: ["HTML", "CSS","Js" ,"BootStrapCss", "Fontawesome"],
+    github_url: "https://github.com/e5lamel5ayed/Disney.git",
+    demo_url: "https://disney-sigma.vercel.app/",
+  },
+  {
+    img: "/Fashion.jpg",
+    title: "Fashion Stock",
+    description:
+      "A Website that displays some clothes with pictures and coordinated form",
+    tech_stack: ["HTML", "CSS","Js" ,"BootStrapCss", "Fontawesome"],
+    github_url: "https://github.com/e5lamel5ayed/fashionstock.git",
+    demo_url: "https://fashionstock.vercel.app/",
+  },
+];
+
+// --------------------------------------- Skills
+
+export const stackList = [
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/html5/html5-original.svg",
+    name: "HTML",
+  },
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/css3/css3-original.svg",
+    name: "CSS",
+  },
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/javascript/javascript-original.svg",
+    name: "JavaScript",
+  },
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/react/react-original.svg",
+    name: "ReactJS",
+  },
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/nextjs/nextjs-original.svg",
+    name: "NextJs",
+  },
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/typescript/typescript-original.svg",
+    name: "Typescript",
+  },
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/git/git-original.svg",
+    name: "Git",
+  },
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/react/react-original.svg",
+    name: "React Native",
+  },
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/nodejs/nodejs-original.svg",
+    name: "NodeJS",
+  },
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/firebase/firebase-plain.svg",
+    name: "Firebase",
+  },
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/mongodb/mongodb-original.svg",
+    name: "MongoDB",
+  },
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg",
+    name: "Python",
+  },
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/flask/flask-original.svg",
+    name: "Flask",
+  },
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/c/c-original.svg",
+    name: "C",
+  },
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/java/java-original.svg",
+    name: "Java",
+  },
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/mysql/mysql-original.svg",
+    name: "MySQL",
+  },
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/bootstrap/bootstrap-original.svg",
+    name: "Bootstrap",
+  },
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/materialui/materialui-original.svg",
+    name: "Material UI",
+  },
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/figma/figma-original.svg",
+    name: "Figma",
+  },
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/tailwindcss/tailwindcss-plain.svg",
+    name: "TailwindCSS",
+  },
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/vscode/vscode-original.svg",
+    name: "VS Code",
+  },
+];
